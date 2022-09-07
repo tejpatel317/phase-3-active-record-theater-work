31ff86b3dc4775df3e33160a94409de862ce4f8e
Tej Patel <tejpatel317@gmail.com>
2022-09-07 10:56:59 -0400

    commit tree 82d11d7a8f16c301c8087304415903e8b57323d7

--- app/models/audition.rb
+++ app/models/audition.rb
@@ -1,3 +1,7 @@
 class Audition < ActiveRecord::Base
-  
+  belongs_to :role
+
+  def call_back
+    self.update_attribute(:hired, true)
+  end
 end
---
+++ app/models/role.rb
@@ -0,0 +1,27 @@
+class Role < ActiveRecord::Base
+    has_many :auditions
+
+    def actors
+        auditions.pluck(:actor)
+    end
+
+    def location
+        auditions.pluck(:location)
+    end
+
+    def lead
+        if auditions.where(hired: true).length > 0
+            auditions.where(hired: true)[0]
+        else
+            'no actor has been hired for this role'
+        end
+    end
+
+    def understudy
+        if auditions.where(hired: true).length > 1
+            auditions.where(hired: true)[1]
+        else
+            'no actor has been hired for understudy for this role'
+        end
+    end
+end
